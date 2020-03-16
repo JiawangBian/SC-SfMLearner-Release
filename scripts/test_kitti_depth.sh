aee87c79fbb55856bf2e8f06f927df4ab2448ef8
bjw <jiawang.bian@gmail.com>
2020-03-16 11:01:19 +1030

    update

--- scripts/test_kitti_depth.sh
+++ scripts/test_kitti_depth.sh
@@ -1,23 +1,14 @@
-# DISP_NET=checkpoints/resnet18_depth_128/10-18-01:01/dispnet_model_best.pth.tar
-# DISP_NET=checkpoints/resnet50_depth_128/10-17-15:53/dispnet_model_best.pth.tar
-
-# DISP_NET=checkpoints/resnet18_depth_256/10-17-13:19/dispnet_model_best.pth.tar
-DISP_NET=checkpoints/resnet50_depth_256/10-17-14:46/dispnet_model_best.pth.tar
+DISP_NET=checkpoints/resnet18_depth_256/10-17-13:19/dispnet_model_best.pth.tar
+# DISP_NET=checkpoints/resnet50_depth_256/10-17-14:46/dispnet_model_best.pth.tar
 
 DATA_ROOT=/media/bjw/Disk/Dataset/kitti_raw/
 RESULTS_DIR=results/test/
 TEST_FILE=kitti_eval/test_files_eigen.txt
 
-
 # test big images
-python test_disp.py --resnet-layers 50 --img-height 256 --img-width 832 \
+python test_disp.py --resnet-layers 18 --img-height 256 --img-width 832 \
 --pretrained-dispnet $DISP_NET --dataset-dir $DATA_ROOT --dataset-list $TEST_FILE \
 --output-dir $RESULTS_DIR
-
-# # test small images
-# python test_disp.py --resnet-layers 50 --img-height 128 --img-width 416 \
-# --pretrained-dispnet $DISP_NET --dataset-dir $DATA_ROOT --dataset-list $TEST_FILE \
-# --output-dir $RESULTS_DIR
 
 
 # evaluate
